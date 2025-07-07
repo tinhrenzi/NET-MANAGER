e01f6829846bfd2aee4f647418b2e9a4d9b79fb4
tinhrenzi <tinhluc02@gmail.com>
2025-07-07 22:31:48 +0700

    first commit

---
+++ NET-MANAGER-main/src/main/java/util/XDialog.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author VINH
+ */
+public class XDialog {
+    public static void alert(String message){
+        XDialog.alert(message, "Thông báo!");
+    }
+    public static void alert(String message, String title){
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.WARNING_MESSAGE);
+    }
+    
+    public static boolean confirm(String message){
+        return XDialog.confirm(message, "Xác nhận!");
+    }
+    public static boolean confirm(String message, String title){
+        int result = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        return (result == JOptionPane.YES_OPTION);
+    }
+    
+    public static String prompt(String message){
+        return XDialog.prompt(message, "Nhập vào!");
+    }
+    public static String prompt(String message, String title){
+        return JOptionPane.showInputDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+}
