24d1a42c130addcab7c0503bdc3365f6e792d944
tinhrenzi <tinhluc02@gmail.com>
2025-07-09 21:10:33 +0700

    first commit

--- NET-MANAGER-main/src/main/java/dao/DoAnDAO.java
+++ NET-MANAGER-main/src/main/java/dao/DoAnDAO.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package dao;
 
@@ -8,6 +8,6 @@ package dao;
  *
  * @author VINH
  */
-public class Categories {
+public interface DoAnDAO {
     
 }
