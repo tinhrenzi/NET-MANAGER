3ce562a72015efa72aca173345c4be5410d19d4d
tinhrenzi <tinhluc02@gmail.com>
2025-07-09 22:45:59 +0700

    Merge branch 'main' of https://github.com/tinhrenzi/NET-MANAGER # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ NET-MANAGER-main/src/main/java/dao/orderDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import entity.Order;
+
+/**
+ *
+ * @author VINH
+ */
+public interface orderDAO extends crudDAO<Order, String>{
+    
+}
