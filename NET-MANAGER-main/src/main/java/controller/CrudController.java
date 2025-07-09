3ba23293af58c7bb5d2eae8f7177b3d4f1f05b6f
NITRO 5 <NITRO 5@luonghiep>
2025-07-09 21:35:18 +0700

    first commit

---
+++ NET-MANAGER-main/src/main/java/controller/CrudController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author VINH
+ */
+public interface CrudController<Entity> {
+    void create();
+    void update();
+    void delete();
+    
+    void setForm(Entity entity); // Hiển thị thực thể lên form
+    Entity getForm(); // Tạo thực thể từ dữ liệu form
+}
