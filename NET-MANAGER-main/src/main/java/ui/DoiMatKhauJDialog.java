e7f3c8be453cff5365df89255773cac8fe3bfff9
tinhrenzi <tinhluc02@gmail.com>
2025-07-10 21:52:04 +0700

    Merge branch 'main' of https://github.com/tinhrenzi/NET-MANAGER # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- NET-MANAGER-main/src/main/java/ui/DoiMatKhauJDialog.java
+++ NET-MANAGER-main/src/main/java/ui/DoiMatKhauJDialog.java
@@ -4,12 +4,14 @@
  *
  * @author Admin
  */
-public class DoiMatKhauJFrame extends javax.swing.JFrame {
+public class DoiMatKhauJDialog extends javax.swing.JDialog {
 
     /**
      * Creates new form Doimatkhau
      */
-    public DoiMatKhauJFrame() {
+    public DoiMatKhauJDialog(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        setLocationRelativeTo(null);
         initComponents();
     }
 
@@ -133,13 +135,13 @@ public class DoiMatKhauJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DoiMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DoiMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DoiMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DoiMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
@@ -149,7 +151,14 @@ public class DoiMatKhauJFrame extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new DoiMatKhauJFrame().setVisible(true);
+                DoiMatKhauJDialog dialog = new DoiMatKhauJDialog(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override 
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
             }
         });
     }
--- NET-MANAGER-main/src/main/java/ui/QuenMatKhauJDialog.java
+++ NET-MANAGER-main/src/main/java/ui/QuenMatKhauJDialog.java
@@ -8,12 +8,14 @@ package ui;
  *
  * @author Admin
  */
-public class QuenMatKhauJFrame extends javax.swing.JFrame {
+public class QuenMatKhauJDialog extends javax.swing.JDialog {
 
     /**
      * Creates new form quyenmatkhau
      */
-    public QuenMatKhauJFrame() {
+    public QuenMatKhauJDialog(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        setLocationRelativeTo(null);
         initComponents();
     }
 
@@ -131,14 +133,18 @@ public class QuenMatKhauJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(QuenMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuenMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(QuenMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuenMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(QuenMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuenMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(QuenMatKhauJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuenMatKhauJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -147,7 +153,14 @@ public class QuenMatKhauJFrame extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new QuenMatKhauJFrame().setVisible(true);
+                QuenMatKhauJDialog dialog = new QuenMatKhauJDialog(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override 
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
             }
         });
     }
