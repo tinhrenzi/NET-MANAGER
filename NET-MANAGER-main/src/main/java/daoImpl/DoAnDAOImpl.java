f236f771cdd9c7871490b19cf5a49059161ec0a2
NITRO 5 <NITRO 5@luonghiep>
2025-07-10 22:43:31 +0700

    first commit

--- NET-MANAGER-main/src/main/java/daoImpl/DoAnDAOImpl.java
+++ NET-MANAGER-main/src/main/java/daoImpl/DoAnDAOImpl.java
@@ -6,10 +6,14 @@ package daoImpl;
 
 import dao.DoAnDAO;
 import entity.DoAn;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import util.XJdbc;
 import util.XQuery;
 
 import java.util.List;
+import static util.XJdbc.openConnection;
 
 public class DoAnDAOImpl implements DoAnDAO {
 
@@ -19,18 +23,24 @@ public class DoAnDAOImpl implements DoAnDAO {
     private final String DELETE_SQL = "DELETE FROM DoAn WHERE MaDoAn=?";
     private final String SELECT_ALL_SQL = "SELECT * FROM DoAn";
     private final String SELECT_BY_ID_SQL = "SELECT * FROM DoAn WHERE MaDoAn=?";
+    private Connection getConnection() throws SQLException {
+        return openConnection();
+    }
 
     @Override
     public DoAn create(DoAn da) {
-        Object[] args = {
-            da.getMaDoan(),
-            da.getTenDoAn(),
-            da.getDonGia(),
-            da.getSoLuong(),
-            da.getTrangThai()
-        };
-        XJdbc.executeUpdate(INSERT_SQL, args);
-        return da;
+        try (Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(INSERT_SQL)) {
+            stmt.setString(1, da.getMaDoan());
+            stmt.setString(2, da.getTenDoAn());
+            stmt.setFloat(3, da.getDonGia());
+            stmt.setInt(4, da.getSoLuong());
+            stmt.setString(5, da.getTrangThai());
+            stmt.executeUpdate();
+            return da;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
