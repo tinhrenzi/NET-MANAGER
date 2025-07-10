b2b41023649488770948b280467f011274733464
tinhrenzi <tinhluc02@gmail.com>
2025-07-10 21:31:23 +0700

    Merge branch 'main' of https://github.com/tinhrenzi/NET-MANAGER # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- NET-MANAGER-main/src/main/java/daoImpl/DoAnDAOImpl.java
+++ NET-MANAGER-main/src/main/java/daoImpl/DoAnDAOImpl.java
@@ -55,12 +55,14 @@ public class DoAnDAOImpl implements DoAnDAO {
 
     @Override
     public void deleteByID(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
     }
 
     @Override
     public DoAn findByID(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
+        return null;
+       
     }
 }
 
